75e79e3cd806ef66eec55ceb7db296911749dd25
Ross F <rfahan@gmail.com>
2023-07-28 00:35:14 -0600

    completed app setup

---
+++ index.js
@@ -0,0 +1,14 @@
+//the main cards and usually get things imported here
+
+
+
+//import packages; import the module from shape.js
+const {Triangle} = require("./lib/shapes.js"); 
+const {Circle} = require("./lib/shapes.js"); 
+const {Square} = srequire("./lib/shapes.js"); 
+
+const {Triangle, Square, Circle } = require("./lib/shapes.js"); 
+//create an array prompt the users for shape, text, shape color and text color
+//create the SVG
+    // 
+    // write the file
