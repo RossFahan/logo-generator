af739fbd57c57740903add517daa2727fbc4f3aa
Ross F <rfahan@gmail.com>
2023-07-28 19:11:31 -0600

    added async prompt for user input

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+//Required
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-// //import packages; import the module from shape.js
+// Import packages; import the module from shape.js
 const { Shape, Triangle, Circle, Square } = require("./lib/shapes.js");
 
-
+// Prompt the user for input
+const inputPrompt = async () => {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo text:',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color keyword or hexadecimal number:',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo:',
+        choices: ['Triangle', 'Circle', 'Square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color keyword or hexadecimal number:',
+      },
+    ]);
+  
+    return answers;
+  };
