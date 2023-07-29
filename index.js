83868407a4dbaf81f1f4da79e6faa576a0b72539
Ross F <rfahan@gmail.com>
2023-07-28 19:38:35 -0600

    added function to Create SVG logo

--- index.js
+++ index.js
@@ -7,30 +7,50 @@ const fs = require('fs');
 const { Shape, Triangle, Circle, Square } = require("./lib/shapes.js");
 
 // Prompt the user for input
-const inputPrompt = async () => {
+const userInput = async () => {
     const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters for the logo text:',
-      },
-      {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter the text color keyword or hexadecimal number:',
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for the logo:',
-        choices: ['Triangle', 'Circle', 'Square'],
-      },
-      {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter the shape color keyword or hexadecimal number:',
-      },
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo text:',
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color keyword or hexadecimal number:',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for the logo:',
+            choices: ['Triangle', 'Circle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color keyword or hexadecimal number:',
+        },
     ]);
-  
+
     return answers;
-  };
+};
+
+// Create the SVG
+const createSVG = (shapeType, shapeColor, text, textColor) => {
+    let shape;
+    switch (shapeType) {
+        case 'Triangle':
+            shape = new Triangle(shapeColor, textColor, text, shapeType);
+            break;
+        case 'Circle':
+            shape = new Circle(shapeColor, textColor, text, shapeType);
+            break;
+        case 'Square':
+            shape = new Square(shapeColor, textColor, text, shapeType);
+            break;
+        default:
+            throw new Error('Error: must select Triangle, Circle, or Square');
+    }
+
+    return shape.render();
+};
